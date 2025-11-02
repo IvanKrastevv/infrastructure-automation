3c8ea6a8ba118376cd2f7ac728e61438c79264a4
IvanKrastevv <142305267+IvanKrastevv@users.noreply.github.com>
2025-11-02 15:18:27 +0000

    Update default prefix and add web_instance_count

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,9 +1,9 @@
-variable "subscription_id" { type = string }
-variable "prefix" { type = string, default = "sslchecker" }
+variable "prefix" { type = string, default = "iacdemo" }
 variable "location" { type = string, default = "westeurope" }
 variable "admin_username" { type = string, default = "azureuser" }
-variable "admin_ssh_key" { type = string }
-# Backend info
+variable "admin_ssh_key" { type = string }      # PUBLIC ssh key content
+variable "subscription_id" { type = string }
 variable "tfstate_rg" { type = string }
 variable "tfstate_sa" { type = string }
 variable "tfstate_container" { type = string, default = "tfstate" }
+variable "web_instance_count" { type = number, default = 2 }
