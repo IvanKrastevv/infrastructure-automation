bce024c5a7869df9a7dd7a3576548a0e82ec4f13
IvanKrastevv <142305267+IvanKrastevv@users.noreply.github.com>
2025-11-02 15:17:50 +0000

    Update Terraform state file key format

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -4,10 +4,11 @@ terraform {
     resource_group_name  = var.tfstate_rg
     storage_account_name = var.tfstate_sa
     container_name       = var.tfstate_container
-    key                  = "${var.prefix}-terraform.tfstate"
+    key                  = "${var.prefix}.terraform.tfstate"
   }
 }
 
 provider "azurerm" {
   features {}
+  subscription_id = var.subscription_id
 }
