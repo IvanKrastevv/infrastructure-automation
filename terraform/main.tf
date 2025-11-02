c295b916e33d746885faa82299a97618856f2488
IvanKrastevv <142305267+IvanKrastevv@users.noreply.github.com>
2025-11-02 15:18:48 +0000

    Update address space and add new subnets

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,12 +7,19 @@ resource "azurerm_virtual_network" "vnet" {
   name                = "${var.prefix}-vnet"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  address_space       = ["10.0.0.0/16"]
+  address_space       = ["10.10.0.0/16"]
 }
 
-resource "azurerm_subnet" "subnet" {
-  name                 = "${var.prefix}-subnet"
+resource "azurerm_subnet" "web" {
+  name                 = "web-subnet"
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = ["10.0.1.0/24"]
+  address_prefixes     = ["10.10.1.0/24"]
+}
+
+resource "azurerm_subnet" "db" {
+  name                 = "db-subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.10.2.0/24"]
 }
