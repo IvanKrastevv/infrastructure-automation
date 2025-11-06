6eb9f48bd56c713d63f90b1bba5908d681b77a89
Ivan Krastev <krastevi@moodys.com>
2025-11-06 16:39:10 +0200

    Fix storage account name + lockfile

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "tfstate-rg"
-    storage_account_name = "tfstatestorage-ivan"
+    storage_account_name = "tfstatestorageivan"
     container_name       = "tfstate"
     key                  = "infra/terraform.tfstate"
   }
